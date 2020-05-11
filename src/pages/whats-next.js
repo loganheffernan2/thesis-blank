73a5f2d801761fe9b052dc91859e81cf3498440b
Logan Heffernan <loganheffernan@logans-mbp.lan>
2020-05-10 22:52:19 -0500

    Whats Next page added

---
+++ src/pages/whats-next.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import '../css/global.css'
+import '../css/next.css'
+
+export default () =>
+
+    <div id="total">
+
+    <p id="whereAreWe">Where Are We?</p>
+        
+    <div class="slide">
+    <p>
+            What's next?
+        </p>
+    </div>
+
+    <div class="slide">
+    <p>
+            "Everything is in everything."
+        </p>
+    </div>
+
+    <div class="slide">
+    <p>
+            Continued publishing.
+        </p>
+    </div>
+
+    <div class="slide">
+    <p>
+            Workshops, working groups.
+        </p>
+    </div>
+
+    <div class="slide">
+    <p>
+            Collaboration and cooperation.
+        </p>
+    </div>
+
+    <div class="slide">
+        <img src="https://previews.123rf.com/images/bsip/bsip1801/bsip180114482/94166369-man-taking-nap.jpg" />
+    </div>
+
+    <div class="slide">
+    <p>
+            Thank you!
+        </p>
+    </div>
+
+</div>
