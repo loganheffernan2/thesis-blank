1a2026152a77dd25951d0bc1ae6e0ea838e165f9
Logan Heffernan <loganheffernan@logans-mbp.lan>
2020-05-24 11:01:57 -0500

    Home summary swap

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,13 @@ export default () =>
       <div id="vertDiv"></div>
       <div id="postContent">
       <span className="headerBumper"></span>
-      <div id="artSum">In Consideration of Ethics and Pedagogy</div>
+      <div id="artSum">An inner-institute research project exploring the positioning of ethics within design practice and pedagogy.</div>
       <div className="titleDiv"></div>
         <div
           id="textContent"
           className="blog-post-content"/>
       <p>
-      <span id="subhead">An inner-institute research project exploring the positioning of ethics within design practice and pedagogy.</span><br/><br/>“Where Are We” was born out of frustration with the limited scope of Pratt Institute’s Communications Design umbrella. Throughout 6 focused semesters, classroom dialogue primarily remained tethered to design as a service to industry. “Design Thinking,” the ethos which produced Silicon Valley libertarianism and eagerly greased the gears of contemporary capitalism, must be decentered  from a design program with ambitions of criticality. As Pratt’s graduates trudge toward marketspaces of global inequity, the institution bears the responsibility of reorienting its pedagogy to help students understand their positionality in both inner- and inter-disciplinary contexts.
+      “Where Are We” was born out of frustration with the limited scope of Pratt Institute’s Communications Design umbrella. Throughout 6 focused semesters, classroom dialogue primarily remained tethered to design as a service to industry. “Design Thinking,” the ethos which produced Silicon Valley libertarianism and eagerly greased the gears of contemporary capitalism, must be decentered  from a design program with ambitions of criticality. As Pratt’s graduates trudge toward marketspaces of global inequity, the institution bears the responsibility of reorienting its pedagogy to help students understand their positionality in both inner- and inter-disciplinary contexts.
       <br/><br/>“Where Are We” is an exercise of hope amidst this daunting landscape—through a series of inner-institute dialogues, the work looks to excavate conversations around ethics and responsibility that are not necessarily awarded syllabus space. The formal output of this project, shifted by coronavirus, has materialized as a digital publishing platform. Pulling reference from Ted Nelson’s insistence on visual connections and with an emphasis on interconnected information, the resultant website provides a resource for both immediate way-finding and speculation on curricular reform. Though initiated as an undergraduate capstone project, I hope to grow “Where Are We” into an on-going collaborative publishing project focused on ethics and pedagogy, an exploration in dislodging design practice from its industrial utility.</p>          
       <img alt="" src="https://raw.githubusercontent.com/loganheffernan2/thesis-blank/master/static/assets/designPolitics-01-01.svg"></img>
       <div id="footerBumper" />
